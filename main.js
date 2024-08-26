5d05109c7d1f4ecef51213cb877dde49a4b7f001
Jose Altuve <119597541+imjosealtuve@users.noreply.github.com>
2024-08-26 08:25:03 -0500

    First commit - Upload app

---
+++ main.js
@@ -0,0 +1,23 @@
+let datosJson;
+        fetch('/js/resumen.json')
+        .then(response => response.json())
+        .then((data) => {
+            datosJson = data;
+            let txtNombre = document.getElementById('nombre');
+            txtNombre.textContent = datosJson.titular;
+            let txtNombre2 = document.getElementById('ahorros');
+            txtNombre2.textContent = datosJson.nro_cuenta;
+            let txtNombre4 = document.getElementById('usd');
+            txtNombre4.textContent = datosJson.saldo[0].monto;
+            let txtNombre5 = document.getElementById('eur');
+            txtNombre5.textContent = datosJson.saldo[1].monto;
+            let txtNombre6 = document.getElementById('cbu');
+            txtNombre6.textContent = datosJson.nro_banco;
+            let txtNombre7 = document.getElementById('abierta');
+            txtNombre7.textContent = datosJson.abierto;
+        })
+        .catch(
+            function(){
+                alert('Error');
+            }
+        )
